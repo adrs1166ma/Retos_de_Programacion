469dfc9a561137ea282892d6364c7069d38464bb
adrs1166ma <163654808+adrs1166ma@users.noreply.github.com>
2024-04-08 12:43:44 -0500

    base

---
+++ Reto_00/reto_00.py
@@ -0,0 +1,42 @@
+#  * EJERCICIO:
+
+#  * - Crea un comentario en el código y coloca la URL del sitio web oficial del
+#  *   lenguaje de programación que has seleccionado.
+#  * - Representa las diferentes sintaxis que existen de crear comentarios
+#  *   en el lenguaje (en una línea, varias...).
+#  * - Crea una variable (y una constante si el lenguaje lo soporta).
+#  * - Crea variables representando todos los tipos de datos primitivos
+#  *   del lenguaje (cadenas de texto, enteros, booleanos...).
+#  * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+#  *
+#  * ¿Fácil? No te preocupes, recuerda que esta es una ruta de estudio y
+#  * debemos comenzar por el principio.
+#  */
+
+#https://www.python.org/
+
+print('estas son las siguientes: ')
+
+"""
+
+1
+
+"""
+'''2'''
+#3
+
+variable = 3
+variable = 4
+#no hay constantes en python
+
+a:str = 'Adrs'
+b:int = 423
+c:float = 15.3
+d:bool = True
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+print("¡Hola, Python!")
