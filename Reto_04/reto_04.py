fa45a60a947f680b5c5d42cea557e8db1c45356e
adrs1166ma <163654808+adrs1166ma@users.noreply.github.com>
2024-04-12 11:39:22 -0500

    Reto 5 completo, reto 4 a medias

--- Reto_04/reto_04.py
+++ Reto_04/reto_04.py
@@ -98,3 +98,41 @@ print(s1.isalnum())
 print(s1.isalpha())
 print(s4.isalpha())
 print(s4.isnumeric())
+
+"""
+Extra
+"""
+
+def check(word1: str, word2: str):
+
+    # Palíndromos
+    print(f"¿{word1} es un palíndromo?: {word1 == word1[::-1]}")
+    print(f"¿{word2} es un palíndromo?: {word2 == word2[::-1]}")
+
+    # Anagramas
+    print(f"¿{word1} es anagrama de {word2}?: {sorted(word1) == sorted(word2)}")
+
+    # Isogramas
+
+    def isogram(word: str) -> bool:
+
+        word_dict = dict()
+        for character in word:
+            word_dict[character] = word_dict.get(character, 0) + 1
+
+        isogram = True
+        values = list(word_dict.values())
+        isogram_len = values[0]
+        for word_count in values:
+            if word_count != isogram_len:
+                isogram = False
+                break
+
+        return isogram
+
+    print(f"¿{word1} es un isograma?: {isogram(word1)}")
+    print(f"¿{word2} es un isograma?: {isogram(word2)}")
+
+
+check("radar", "pythonpythonpythonpython")
+# check("amor", "roma")
